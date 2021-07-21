77006113550ddff795e6c60842d7ad42bc408688
A.C.Kimari <annchristine704@gmail.com>
2021-07-21 15:35:41 -0700

    defined Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+function Employee(Name, Role, Id, Email, OfficeNumber, Github, School) {
+    this.name = Name
+    this.role = Role
+    this.id = Id
+    this.email = Email
+    this.officeNumber = OfficeNumber
+    this.github = Github
+    this.school = School
+};
+
+var engineer = new Employee
+engineer.name = "";
+engineer.role = "";
+engineer.id = "";
+engineer.email = "";
+engineer.github = "";
+
+var manager = new Employee
+manager.name = "";
+manager.role = "";
+manager.id = "";
+manager.email = "";
+manager.officeNumber = "";
+
+var intern = new Employee
+intern.name = "";
+intern.role = "";
+intern.id = "";
+intern.email = "";
+intern.school = "";
